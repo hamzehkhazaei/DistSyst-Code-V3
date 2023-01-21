8ca6f1b816a4bc519412c9bc418985dea54387c1
hamzehkhazaei <hkh@yorku.ca>
2023-01-20 23:42:35 -0500

    init

--- 02-03/client.py
+++ 02-03/client.py
@@ -5,5 +5,5 @@ s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
 s.send('Hello, world')  # send some data
 data = s.recv(1024)     # receive the response
-print data              # print the result
+print data
 s.close()               # close the connection
